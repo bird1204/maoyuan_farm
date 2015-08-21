ed7fe034f3a44dd351308ad8b6fccdb4c075e84e
bird Chiu <bird1204@gmail.com>
2015-08-22 01:28:39 +0800

    整理view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :active_page
+  def active_page
+    path = Rails.application.routes.recognize_path(request.env['PATH_INFO'])
+    @controller = path[:controller]
+    # action = path[:action]
+    p @controller
+  end
 end
