c25fbebfe66a692066e7c854463f33c691fa2ebc
bird Chiu <bird1204@gmail.com>
2015-09-03 23:29:19 +0800

    開開最帥高高最酷ＢＢ最屁....

--- app/assets/javascripts/tracking_lists.coffee
+++ app/assets/javascripts/tracking_lists.coffee
@@ -1,3 +1,13 @@
 # Place all the behaviors and hooks related to the matching controller here.
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://coffeescript.org/
+
+$(document).on 'click', ':button[type="cart_del"]', ->
+  console.log @type
+  $.ajax
+    url: '/tracking_lists/' + @id.split('_')[1]
+    method: 'DELETE'
+    success: (data) ->
+      $('.cart-table').html data
+      return
+  return
--- app/controllers/tracking_lists_controller.rb
+++ app/controllers/tracking_lists_controller.rb
@@ -5,11 +5,15 @@ class TrackingListsController < ApplicationController
   end
 
   def create
-    current_user.tracking_lists.build(product_id: params[:product_id]).save!
+    current_user.tracking_lists.find_or_initialize_by(product_id: params[:product_id]).save!
     redirect_to :back, notice: "成功加入一筆商品至追蹤清單"
   end
 
   def destroy
+    TrackingList.find(params[:id]).destroy!
 
+    respond_to do |format|
+      format.html { render layout: false }
+    end
   end
 end
