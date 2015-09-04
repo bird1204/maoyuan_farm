d09a89d64e6fa8ffb4044c0442cedf0d184caa0c
徐章嘉 <g8071107@gmail.com>
2015-09-04 20:57:51 +0800

    Merge branch 'master' into mafia-dev

--- app/controllers/tracking_lists_controller.rb
+++ app/controllers/tracking_lists_controller.rb
@@ -1,6 +1,19 @@
 class TrackingListsController < ApplicationController
   before_action :authenticate_user!
   def index
-    @lists = TrackingList.all.page(params[:page])
+    @tracking_lists = current_user.tracking_lists.includes(:product).page(params[:page])
+  end
+
+  def create
+    current_user.tracking_lists.find_or_initialize_by(product_id: params[:product_id]).save!
+    redirect_to :back, notice: "成功加入一筆商品至追蹤清單"
+  end
+
+  def destroy
+    TrackingList.find(params[:id]).destroy!
+
+    respond_to do |format|
+      format.html { render layout: false }
+    end
   end
 end
--- app/models/tracking_list.rb
+++ app/models/tracking_list.rb
@@ -1,3 +1,5 @@
 class TrackingList < ActiveRecord::Base
   belongs_to :product
+  belongs_to :user
+  self.per_page = 5
 end
