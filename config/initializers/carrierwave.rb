219159c09da98289fa044d73f061387ae171c04c
bird Chiu <bird1204@gmail.com>
2015-09-07 21:43:04 +0800

    111

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,16 +1,31 @@
 require 'yaml'
 CarrierWave.configure do |config|
-  aws = YAML.load_file("#{Rails.root.to_s}/config/aws.yml")
-  config.fog_provider = 'fog/aws'                        # required
-  config.fog_credentials = {
-    provider:              'AWS',                        # required
-    aws_access_key_id:     aws[:key_id],                        # required
-    aws_secret_access_key: aws[:access_key],                    # required
-    region:                aws[:s3][:region],                  # optional, defaults to 'us-east-1'
-    # host:                  's3.example.com',             # optional, defaults to nil
-    # endpoint:              'https://s3.example.com:8080' # optional, defaults to nil
+  # ENV = YAML.load_file("#{Rails.root.to_s}/config/aws.yml")
+
+  config.storage    = :aws
+  config.aws_bucket = ENV['S3_BUCKET']
+  config.aws_acl    = 'public-read'
+
+  # Optionally define an asset host for configurations that are fronted by a
+  # content host, such as CloudFront.
+  # config.asset_host = 'http://example.com'
+
+  # The maximum period for authenticated_urls is only 7 days.
+  config.aws_authenticated_url_expiration = 60 * 60 * 24 * 7
+
+  # Set custom options such as cache control to leverage browser caching
+  config.aws_attributes = {
+    expires: 1.week.from_now.httpdate,
+    cache_control: 'max-age=604800'
   }
-  config.fog_directory  = aws[:s3][:bucket]                          # required
-  config.fog_public     = false                                        # optional, defaults to true
-  config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" } # optional, defaults to {}
+
+  config.aws_credentials = {
+    access_key_id:     ENV['ACCESS_KEY_ID'],
+    secret_access_key: ENV['SECRET_ACCESS_KEY'],
+    region:            ENV['S3_REGION'] # Required
+  }
+
+  # Optional: Signing of download urls, e.g. for serving private
+  # content through CloudFront.
+  # config.aws_signer = -> (unsigned_url, options) { Aws::CF::Signer.sign_url unsigned_url, options }
 end
