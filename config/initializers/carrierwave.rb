cbcf073245c7826e9fa347cc2f1658cf85d850d2
bird Chiu <bird1204@gmail.com>
2015-09-07 22:33:26 +0800

    bug fix

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,16 +1,16 @@
 require 'yaml'
 CarrierWave.configure do |config|
-  aws = YAML.load_file("#{Rails.root.to_s}/config/aws.yml")
+  # aws = YAML.load_file("#{Rails.root.to_s}/config/aws.yml")
   config.fog_provider = 'fog/aws'                        # required
   config.fog_credentials = {
     provider:              'AWS',                        # required
-    aws_access_key_id:     aws[:key_id],                        # required
-    aws_secret_access_key: aws[:access_key],                    # required
-    region:                aws[:s3][:region],                  # optional, defaults to 'us-east-1'
+    aws_access_key_id:     ENV['ACCESS_KEY_ID'],                        # required
+    aws_secret_access_key: ENV['SECRET_ACCESS_KEY'],                    # required
+    region:                ENV['S3_REGION'],                  # optional, defaults to 'us-east-1'
     # host:                  's3.example.com',             # optional, defaults to nil
     # endpoint:              'https://s3.example.com:8080' # optional, defaults to nil
   }
-  config.fog_directory  = aws[:s3][:bucket]                          # required
+  config.fog_directory  = ENV['S3_BUCKET']                          # required
   config.fog_public     = false                                        # optional, defaults to true
   config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" } # optional, defaults to {}
 end
